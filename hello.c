39e9ad157b76fb713fdf9ec59744043ab045e2a4
Ibrahim-Dii <240023018@dii.tj>
2026-02-01 20:29:33 +0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* 
+ * The classic "hello, world" program
+ * From K&R page 6
+ */
+
+main()
+{
+    printf("hello, world\n");
+}
---
+++ hello_multiple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Multiple printf statements to build output
+ * Demonstrates that printf doesn't add newlines automatically
+ */
+
+main()
+{
+    printf("hello, ");
+    printf("world");
+    printf("\n");
+}
